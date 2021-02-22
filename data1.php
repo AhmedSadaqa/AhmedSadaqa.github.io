0955f25b84716c01bb488282427869b340648489
AhmedSadaqa <64624614+AhmedSadaqa@users.noreply.github.com>
2021-02-23 00:56:23 +0200

    Add files via upload

---
+++ data1.php
@@ -0,0 +1,22 @@
+{
+    "meta": {
+        "page": 1,
+        "pages": 1,
+        "perpage": -1,
+        "total": 350,
+        "sort": "asc",
+        "field": "RecordID"
+    },
+    "data": [
+    {
+        "RecordID": 1,
+        "Queue" : "Couner1"
+        "CreatedAt": "12/31/1999",
+        "Categories": "Cash Transaction"
+        "Status": 5,
+        "Type": 1
+    }
+
+    ]
+
+}
